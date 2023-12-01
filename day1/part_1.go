a21b4976ef814db0988c85f1a44efc7e3eeee602
zowber <andy@zowber.com>
2023-12-01 20:38:39 +0000

    add input

--- day1/part_1.go
+++ day1/part_1.go
@@ -1,1 +1,15 @@
 package day1
+
+import (
+	"log"
+	"os"
+)
+
+func readInput() {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        log.Println("Err reading input file")
+    }
+
+    log.Println(file)
+}
